3f6a3f2c727c4aac1596539265fbbe595da24911
Fausto Palma <faustopalma13@gmail.com>
2022-09-08 16:22:48 -0500

    fix

--- src/MoviesGrid.jsx
+++ src/MoviesGrid.jsx
@@ -3,9 +3,9 @@ import movies from "./movies.json";
 export function MoviesGrid() {
   return (
     <ul>
-      {movies.map(function (movie) {
-        return <li>{movie.title}</li>;
-      })}
+      {movies.map((movie) => (
+      	<li key={movies.id}>{movie.title}</li>
+      ))}
     </ul>
   );
 }
