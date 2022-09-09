371d4700510bcdf183fcd9f5576b3532550dc5e6
Fausto Palma <faustopalma13@gmail.com>
2022-09-09 00:25:53 -0500

    agregamos fetch y usamos los datos remotos para actualizar la pagina de catalogo y peliculas

--- src/Components/MoviesGrid.jsx
+++ src/Components/MoviesGrid.jsx
@@ -1,12 +1,21 @@
-import { MovieCard } from "./MovieCard";
-import movies from "./movies.json";
-import styles from './MoviesGrid.module.css'
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { get } from '../utils/httpClient';
+import { MovieCard } from './MovieCard';
+import styles from './MoviesGrid.module.css';
 
 export function MoviesGrid() {
+  const [movies, setMovies] = useState([]);
+  
+  useEffect(() => {
+    get('/discover/movie').then(data => {
+      setMovies(data.results);
+    })
+}, []);
   return (
     <ul className={styles.moviesGrid}>
       {movies.map((movie) => (
-      	<MovieCard key={movie.id} movie={movie}/>
+        <MovieCard key={movie.id} movie={movie} />
       ))}
     </ul>
   );
---
+++ src/utils/httpClient.js
@@ -0,0 +1,11 @@
+const API = "https://api.themoviedb.org/3";
+
+export function get(path) {
+  return fetch(API + path, {
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYjI1MDY1YmRiZmViYzQ5MTRkOTBhMmUyN2NiMDczYSIsInN1YiI6IjYzMWFiZDcxMDk3YzQ5MDA3ZmIzN2ZiMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jsuAYFv2MaOI0PKTB2aQjRoPcbn8W9yuyU__QqekeDM",
+      "Content-Type": "application/json;charset=utf-8",
+    },
+  }).then((result) => result.json());
+}
