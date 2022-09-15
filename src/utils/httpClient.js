55486baa7027631b929477bb24e376a814ca4f55
Fausto Palma <faustopalma13@gmail.com>
2022-09-15 13:17:48 -0500

    variables locales agregadas

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,10 +1,11 @@
-const API = "https://api.themoviedb.org/3";
+const API = process.env.REACT_APP_API;
+const API_TOKEN = process.env.REACT_APP_API_TOKEN;
 
 export function get(path) {
   return fetch(API + path, {
     headers: {
       Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYjI1MDY1YmRiZmViYzQ5MTRkOTBhMmUyN2NiMDczYSIsInN1YiI6IjYzMWFiZDcxMDk3YzQ5MDA3ZmIzN2ZiMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jsuAYFv2MaOI0PKTB2a",
+        "Bearer " + API_TOKEN,
       "Content-Type": "application/json;charset=utf-8",
     },
   }).then((result) => result.json());
